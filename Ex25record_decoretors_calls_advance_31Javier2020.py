19d11317a794f814ca4844ce372bf4f3577903d7
Teodor Wisniewski <teodow.wisniewski@mail.nidec.com>
2020-01-31 17:26:49 +0100

    record_calls decorathors

--- Ex25record_decoretors_calls_advance_31Javier2020.py
+++ Ex25record_decoretors_calls_advance_31Javier2020.py
@@ -1,5 +1,4 @@
 
-
 class record_calls(object):
 
     def __init__(self,initial_function):
@@ -9,7 +8,6 @@ class record_calls(object):
 
     def __call__(self,*args,**kwargs):
         print("To jest przed wywolaniem orginalnej classy")
-        self.call_count +=1
         return self.initial_function(*args,**kwargs)
 
 @record_calls
@@ -17,17 +15,33 @@ def greet(name):
     """Greet someone by their name."""
     print(f"Hello {name}")
 
+def arg_dec(html_sign):
+    def p_decorate(funkja):
+        def funkcja_wrapper(self):
+            return html_sign + funkja(self).casefold() + html_sign.replace("<","</")
+        return funkcja_wrapper
+    return p_decorate
+
+class Person(object):
+    def __init__(self):
+        self.name = "John"
+        self.family = "Doe"
+
+    @arg_dec("<p>")
+    def get_fullname(self):
+        return self.name+" "+self.family
 
 
 
 if __name__=="__main__":
     greet("Joghnn")
-
+    my_person = Person()
+    print(my_person.get_fullname())
     greet("Trey")
 
     print(greet.call_count)
 # 1
-    greet()
+    greet("world")
 # Hello world
     print(greet.call_count)
 # 2
