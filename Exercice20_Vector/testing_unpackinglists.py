6112963b1599a6ab65c9a078bf00d911c5200cee
Teodor Wisniewski <teodorwis90@gmail.com>
2020-07-26 11:14:11 +0200

    unpacking lists - readable and quick

---
+++ Exercice20_Vector/testing_unpackinglists.py
@@ -0,0 +1,15 @@
+
+import datetime
+
+today_date = str(datetime.date.today())
+
+print(today_date)
+print(type(today_date))
+
+splitted_date = today_date.split("-")
+year,month,day = splitted_date
+
+print(f"It is {year}. The day is {day} and the month is {month} ")
+
+# Conclusion: we can unpack lists! It is more readable than spllited_date[0] and so on!
+
