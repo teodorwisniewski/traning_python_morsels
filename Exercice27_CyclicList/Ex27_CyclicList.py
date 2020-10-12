2846d3c5090ab10480fcf18ef016f3a8a9eaddca
Teodor Wisniewski <teodow.wisniewski@mail.nidec.com>
2020-10-12 16:45:46 +0200

    test for a base problem 27, creating iterators

--- Exercice27_CyclicList/Ex27_CyclicList.py
+++ Exercice27_CyclicList/Ex27_CyclicList.py
@@ -2,12 +2,30 @@
 from typing import Iterable, Any
 
 
-def CyclicList(liste: Iterable) ->Any:
-    index = 0
-    while True:
-        if index >= len(liste):
-            index = 0
-        yield liste[index]
-        index += 1
+class CyclicListIterator:
 
+    def __init__(self, liste: Iterable):
+        self.liste = liste
+        self.index = 0
+
+    def __next__(self):
+        while True:
+            if self.index >= len(self.liste):
+                self.index = 0
+            value = self.liste[self.index]
+            self.index += 1
+            return value
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+
+class CyclicList:
+
+    def __init__(self, liste: Iterable):
+        self.liste = list(liste)
+
+    def __iter__(self):
+        return CyclicListIterator(self.liste)
 
--- Exercice27_CyclicList/tests/test_Ex27_CyclicList.py
+++ Exercice27_CyclicList/tests/test_Ex27_CyclicList.py
@@ -10,3 +10,13 @@ def test_base_problem():
         if i > 8:
             break
     assert list(islice(my_list, 5)) == [1, 2, 3, 1, 2]
+    
+    
+def test_base_problem_idependent_lists():
+    numbers = CyclicList([1, 2, 3, 4])
+    i1 = iter(numbers)
+    i2 = iter(numbers)
+    assert next(i1) == 1
+    assert next(i1) == 2
+    assert next(i2) == 1
+    assert next(i2) == 2
