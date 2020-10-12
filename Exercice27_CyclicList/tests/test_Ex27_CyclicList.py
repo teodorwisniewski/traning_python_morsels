0f2243b3a1f6ed422c5655b2a328f9346ed518b5
Teodor Wisniewski <teodow.wisniewski@mail.nidec.com>
2020-10-12 16:14:51 +0200

    test for a base problem 27

--- Exercice27_CyclicList/tests/test_Ex27_CyclicList.py
+++ Exercice27_CyclicList/tests/test_Ex27_CyclicList.py
@@ -1,1 +1,12 @@
 import pytest
+from itertools import islice
+from Exercice27_CyclicList.Ex27_CyclicList import CyclicList
+
+
+def test_base_problem():
+    my_list = CyclicList([1, 2, 3])
+    for i, n in enumerate(my_list):
+        print(n)
+        if i > 8:
+            break
+    assert list(islice(my_list, 5)) == [1, 2, 3, 1, 2]
