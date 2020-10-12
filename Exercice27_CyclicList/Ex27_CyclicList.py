cd4a099d1b3cec4968f58c3951dd02c6b351ab0c
Teodor Wisniewski <teodow.wisniewski@mail.nidec.com>
2020-10-12 16:11:46 +0200

    CyclicList base problem

--- Exercice27_CyclicList/Ex27_CyclicList.py
+++ Exercice27_CyclicList/Ex27_CyclicList.py
@@ -1,1 +1,13 @@
 # CyclicList
+from typing import Iterable, Any
+
+
+def CyclicList(liste: Iterable) ->Any:
+    index = 0
+    while True:
+        if index >= len(liste):
+            index = 0
+        yield liste[index]
+        index += 1
+
+
