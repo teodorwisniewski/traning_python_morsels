4524d381f7bdba2d1630e9239d266901eab8ed86
Teodor Wisniewski <teodow.wisniewski@mail.nidec.com>
2020-10-14 15:11:49 +0200

    Solution 4 collections UserList self.data

--- Exercice27_CyclicList/Ex27_CyclicList_Solution4.py
+++ Exercice27_CyclicList/Ex27_CyclicList_Solution4.py
@@ -1,9 +1,16 @@
 from collections import UserList
+from itertools import cycle
+#
+# class CyclicList(list):
+#
+#     def __iter__(self):
+#         index = 0
+#         while True:
+#             yield self[index]
+#             index = (index+1) % len(self)
 
-class CyclicList(list):
+
+class CyclicList(UserList):
 
     def __iter__(self):
-        index = 0
-        while True:
-            yield self[index]
-            index = (index+1) % len(self)
+        return cycle(self.data)
