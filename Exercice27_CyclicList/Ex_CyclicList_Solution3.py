02ac340e2c04403014fc137bf5c3c364c2376f37
Teodor Wisniewski <teodow.wisniewski@mail.nidec.com>
2020-10-14 14:04:16 +0200

    implementing pop, append and __len__ methods

--- Exercice27_CyclicList/Ex_CyclicList_Solution3.py
+++ Exercice27_CyclicList/Ex_CyclicList_Solution3.py
@@ -8,3 +8,14 @@ class CyclicList:
 
     def __iter__(self):
         return cycle(self.data)
+
+    def append(self,val):
+        self.data.append(val)
+
+    def pop(self,index=None):
+        if index is None:
+            return self.data.pop()
+        return self.data.pop(index)
+
+    def __len__(self):
+        return len(self.data)
